6fb0f264cf6110c6e1ebf75e859d0b4c4b2728f1
ramaika108 <ramaika108@gmail.com>
2020-08-24 15:18:55 +0530

    Fixed indexes issue

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ def read_from_file(book_name="sorted_bg.json"):
     return BG
 
 def main():
-    chapter_number = int(sys.argv[1])
-    text_number = int(sys.argv[2])
+    chapter_number = int(sys.argv[1])-1
+    text_number = int(sys.argv[2])-1
 
     BG = read_from_file()
     
     chapters = [chapter for chapter in BG.keys()]
 
     requested_text = BG[chapters[chapter_number]][text_number]
+    print(chapters[chapter_number], end='\n\n')
     for content in requested_text.values():
         print(content, end='\n\n')
     
