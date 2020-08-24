00ac02dc2728eccb6c6e401675db19d42f33a714
ramaika108 <ramaika108@gmail.com>
2020-08-24 19:06:14 +0530

    Now gets all indexes except this type. "31-33" will not work for 32.

--- main.py
+++ main.py
@@ -8,23 +8,43 @@ def read_from_file(book_name="sorted_bg.json"):
     return BG
 
 def main():
-    if int(sys.argv[1])-1 > 17:
-        chapter_number = 17
-    else:
-        chapter_number = int(sys.argv[1])-1
-    text_number = int(sys.argv[2])-1
 
     BG = read_from_file()
-    
-    chapters = [chapter for chapter in BG.keys()]
+    chapter_number = int(sys.argv[1])-1
+    argument_2 = int(sys.argv[2])
 
-    
-    requested_text = BG[chapters[chapter_number]][text_number]
+    chapter_number = (17 if chapter_number > 17 else chapter_number)
+    chapters = [chapter for chapter in BG.keys()]
+    sellected_chapter = BG[chapters[chapter_number]]
     print(chapters[chapter_number], end='\n\n')
-    for content in requested_text.values():
-        print(content, end='\n\n')
-    print(len(BG[chapters[chapter_number]]))
-    print(text_number, chapter_number)
+
+    temp_text_number = None
+    if argument_2 > int(sellected_chapter[-1]['number'][-3:-1]):
+        print('This text does not exist in ', chapters[chapter_number])
+        text_number = len(sellected_chapter) - 1
+        print(sellected_chapter[text_number])
+        return
+    elif argument_2 >= len(sellected_chapter):
+        temp_text_number = len(sellected_chapter)-1
+        print('This text exists in chapter but is not in array range.')
+    else:
+        temp_text_number = argument_2 - 1
+
+    print('initiating search at:', temp_text_number)
+    print('length of chapter array:', len(sellected_chapter))
+    print(sellected_chapter[temp_text_number]['number'])
+    while str(argument_2) not in sellected_chapter[temp_text_number]['number']:
+        temp_text_number -= 1
+        print(sellected_chapter[temp_text_number]['number'], argument_2)
+        print(str(argument_2) in sellected_chapter[temp_text_number]['number'])
+    print(temp_text_number)
+    text_number = temp_text_number
+    print(sellected_chapter[text_number])
+    
+
+
+#    for content in requested_text.values():
+#        print(content, end='\n\n')
     
 
 if __name__ == '__main__':
