910d57d78a6ac837efdf7c2dd94f375da1c9582b
ramaika108 <ramaika108@gmail.com>
2020-08-24 15:53:11 +0530

    Fixed chapter out of range issue, now if chapter is out of range chapter 18 is sellected

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ def read_from_file(book_name="sorted_bg.json"):
     return BG
 
 def main():
-    chapter_number = int(sys.argv[1])-1
+    if int(sys.argv[1])-1 > 17:
+        chapter_number = 17
+    else:
+        chapter_number = int(sys.argv[1])-1
     text_number = int(sys.argv[2])-1
 
     BG = read_from_file()
     
     chapters = [chapter for chapter in BG.keys()]
 
+    
     requested_text = BG[chapters[chapter_number]][text_number]
     print(chapters[chapter_number], end='\n\n')
     for content in requested_text.values():
         print(content, end='\n\n')
+    print(len(BG[chapters[chapter_number]]))
+    print(text_number, chapter_number)
     
 
 if __name__ == '__main__':
